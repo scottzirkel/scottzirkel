928cf651ef7fffedba4f7babbfb3f756067966d9
Scott Zirkel <scott@scottzirkel.com>
2018-06-28 08:25:23 -0500

    Added GA
    Oops

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -75,6 +75,15 @@
       <p>Copyright © 2003 – {{ date('Y') }} Scott Zirkel. All Rights Reserved.</p>
     </footer>
     @yield('scripts')
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-144747-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-144747-1');
+    </script>
   </body>
 </html>
 
