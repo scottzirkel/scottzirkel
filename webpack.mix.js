5935a3c6c0e5b5d7d633c5df263d4e73064f19b5
Scott Zirkel <scott@scottzirkel.com>
2019-06-14 19:10:23 -0500

    Added critical css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,37 @@
 const mix = require('laravel-mix')
-const precss = require('precss')
 const tailwindcss = require('tailwindcss')
 require('laravel-mix-purgecss')
+require('laravel-mix-criticalcss')
 
 mix.postCss('resources/assets/css/base.css', 'public/css/main.css', [
-  precss(),
   tailwindcss('tailwind.js')
 ])
-
-// mix.copy('resources/assets/images', 'public/images', false)
+.criticalCss({
+  enabled: mix.inProduction(),
+  paths: {
+      base: 'http://scottzirkel.test',
+      templates: 'public/css/critical/'
+  },
+  urls: [
+      {
+          url: '/',
+          template: 'home'
+      }
+  ]
+})
 
 if (mix.inProduction()) {
-  mix.purgeCss()
+  mix
+  .purgeCss({
+      enabled: true,
+      globs: [
+          path.join(__dirname, 'layouts/*.html'),
+          path.join(__dirname, 'templates/**/*.html'),
+          path.join(__dirname, 'partials/**/*.html'),
+          path.join(__dirname, 'js/**.js'),
+          path.join(__dirname, 'img/**.svg'),
+      ],
+      extensions: ['html', 'js', 'php', 'vue', 'svg'],
+  })
+  .sourceMaps()
 }
-
-// mix.js('resources/assets/js/admin/admin.js', 'public/js')
-
-// mix.js('resources/assets/js/app.js', 'public/js')
-// .extract(['vue', 'axios', 'vue2-leaflet'])
