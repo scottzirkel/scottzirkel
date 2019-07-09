6648caf86c813764e42e171b9959e72d5b25b1ce
Scott Zirkel <scott@scottzirkel.com>
2019-07-09 16:05:55 -0500

    Added favicons

--- src/main.js
+++ src/main.js
@@ -44,4 +44,55 @@ export default function (Vue, { router, head, isClient }) {
     name: 'twitter:creator',
     content: '@scottzirkel'
   })
+
+  head.meta.push({
+    name: 'msapplication-TileColor',
+    content: '#b64243'
+  })
+
+  head.meta.push({
+    name: 'msapplication-config',
+    content: '/icons/browserconfig.xml'
+  })
+
+  head.meta.push({
+    name: 'theme-color',
+    content: '#fbfbfa'
+  })
+
+  head.link.push({
+    rel: 'apple-touch-icon',
+    sizes: '180x180',
+    href: '/icons/apple-touch-icon.png'
+  })
+
+  head.link.push({
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/icons/favicon-32x32.png'
+  })
+
+  head.link.push({
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: '/icons/favicon-16x16.png'
+  })
+
+  head.link.push({
+    rel: 'manifest',
+    href: '/icons/site.webmanifest'
+  })
+
+  head.link.push({
+    rel: 'mask-icon',
+    href: '/icons/safari-pinned-tab.svg',
+    color: '#fbfbfa'
+  })
+
+  head.link.push({
+    rel: 'shortcut-icon',
+    href: '/icons/favicon.ico'
+  })
 }
