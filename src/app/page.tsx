3e0e3bbfeb0df0db313211206ec629cddf8779d8
Scott Zirkel <scott@scottzirkel.com>
2023-02-08 19:27:15 -0600

    File formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 export default function Home() {
-  return (
-    <>
-      <p>Hey gang, I'm Scott and this is my site (obviously). I'm an artist, a programmer, a writer, a designer, a father, a husband, a Jeeper, an overlander, and probably a dozen other things I've forgotten.</p>
-      <p>I've got a few drawings, some writing, and other things scattered throughout the site and across various social media platforms. I do hope you find something you enjoy.</p>
-      <p>Thanks, Scott</p>
-    </>
-  )
+	return (
+		<>
+			<p>
+				Hey gang, I'm Scott and this is my site (obviously). I'm an artist, a programmer, a writer, a designer, a father, a
+				husband, a Jeeper, an overlander, and probably a dozen other things I've forgotten.
+			</p>
+			<p>
+				I've got a few drawings, some writing, and other things scattered throughout the site and across various social media
+				platforms. I do hope you find something you enjoy.
+			</p>
+			<p>Thanks, Scott</p>
+		</>
+	)
 }
