488306f6faa4cf08e3c6acdab05b0efe746b6037
Scott Zirkel <scott@scottzirkel.com>
2023-02-21 15:30:57 -0600

    Added more head tag stuff to the main pages

---
+++ src/app/design/head.tsx
@@ -0,0 +1,28 @@
+export default function Head() {
+	return (
+		<>
+			<title>Designs by Scott Zirkel</title>
+			<meta content="width=device-width, initial-scale=1" name="viewport" />
+			<meta name="description" content="Logos, sites, and more." />
+			<link rel="icon" href="/favicon.ico" />
+			<meta property="og:url" content="https://scottzirkel.com/designs" />
+			<meta property="og:type" content="website" />
+			<meta property="og:title" content="Designs by Scott Zirkel" />
+			<meta property="og:description" content="Logos, sites, and more." />
+			<meta
+				property="og:image"
+				content="https://imagedelivery.net/ss7-gAVfWWJP_LdCZpUXVQ/2c84e5f7-9561-4a3e-84a0-f5b2140c5f00/og"
+			/>
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta property="twitter:domain" content="scottzirkel.com" />
+			<meta property="twitter:url" content="https://scottzirkel.com/designs" />
+			<meta name="twitter:title" content="Designs by Scott Zirkel" />
+			<meta name="twitter:description" content="Logos, sites, and more." />
+			<meta
+				name="twitter:image"
+				content="https://imagedelivery.net/ss7-gAVfWWJP_LdCZpUXVQ/2c84e5f7-9561-4a3e-84a0-f5b2140c5f00/og"
+			/>
+		</>
+	)
+}
+
