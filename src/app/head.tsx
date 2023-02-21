363c40115adff28babdb35421981d913443ea7d2
Scott Zirkel <scott@scottzirkel.com>
2023-02-21 15:05:55 -0600

    Updated og info

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,6 +5,23 @@ export default function Head() {
 			<meta content="width=device-width, initial-scale=1" name="viewport" />
 			<meta name="description" content="Artist, author, developer, designer. Making stuff up since 1977" />
 			<link rel="icon" href="/favicon.ico" />
+			<meta property="og:url" content="https://scottzirkel.com" />
+			<meta property="og:type" content="website" />
+			<meta property="og:title" content="Scott Zirkel" />
+			<meta property="og:description" content="Artist, author, developer, designer. Making stuff up since 1977" />
+			<meta
+				property="og:image"
+				content="https://imagedelivery.net/ss7-gAVfWWJP_LdCZpUXVQ/2c84e5f7-9561-4a3e-84a0-f5b2140c5f00/og"
+			/>
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta property="twitter:domain" content="scottzirkel.com" />
+			<meta property="twitter:url" content="https://scottzirkel.com" />
+			<meta name="twitter:title" content="Scott Zirkel" />
+			<meta name="twitter:description" content="Artist, author, developer, designer. Making stuff up since 1977" />
+			<meta
+				name="twitter:image"
+				content="https://imagedelivery.net/ss7-gAVfWWJP_LdCZpUXVQ/2c84e5f7-9561-4a3e-84a0-f5b2140c5f00/og"
+			/>
 		</>
 	)
 }
