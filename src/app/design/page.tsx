6da63bb6c0d6bd5c2761ee5bfa1f05a3df17e6d8
Scott Zirkel <scott@scottzirkel.com>
2024-05-22 17:39:16 -0500

    Did this work?

--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -14,19 +14,29 @@ export default async function Page() {
 			<PageTitle>Design</PageTitle>
 			<p>
 				I studied design in college and started working as a designer after graduation. I worked on a multitude of projects for
-				clients of all sizes. After more than 20 years, I realized I didn't enjoy it as much as I used to. I accomplished a lot,
-				including being able to do package design for a Star Wars licensed product.
+				clients of all sizes. After{' '}
+				<mark className="inline-block rotate-1 bg-red-500 px-1 text-gray-50">
+					<span className="inline-block -rotate-1">more than 20 years,</span>
+				</mark>{' '}
+				I realized I didn't enjoy it as much as I used to. I accomplished a lot, including being able to do package design for a
+				Star Wars licensed product.
 			</p>
 			<p>
 				Fortunately while in college, one of my professors introduced me to HTML, enforcing the importance of learning to hand
-				code, and not rely on tools to build for you – which at the time was FrontPage.
+				code, and not rely on tools to build for you – which at the time was{' '}
+				<mark className="inline-block -rotate-3 bg-red-500 px-1 text-gray-50">
+					<span className="inline-block rotate-3">FrontPage.</span>
+				</mark>
 			</p>
 			<p>
 				I found quickly that I enjoyed writing code, and was one of few designers that could also code. Not only did I know HTML
 				& CSS, but soon picked up PHP and JavaScript as well as SQL, database and server management. I'm fortunate to be able to
 				choose between careers, and was able to pivot to development only quite easily.
 			</p>
-			<p>I still enjoy designing for myself, but for the most part, I'm done with client work.</p>
+			<p>
+				I still enjoy designing for myself, but for the most part,{' '}
+				<u className="text-gray-900 decoration-red-500 decoration-2">I'm no longer taking on client design work.</u>
+			</p>
 			<PageSubtitle>Dribbble</PageSubtitle>
 			<Dribbble />
 			<p>
@@ -42,9 +52,9 @@ export default async function Page() {
 				work with a variety of clients on a myriad of projects.
 			</p>
 			<p>Here are a few of my favorite from over the years:</p>
-			<p>
-				(Drop in logos, package design, web design, etc, BEFORE launch. Otherwise people will see this and think you're a
-				poser.)
+			<p className="border-l-4 border-l-red-500 bg-gray-200 pl-4 font-mono text-lg">
+				TODO: Drop in logos, package design, web design, etc, BEFORE launch. Otherwise people will see this and think you're a
+				poser.
 			</p>
 		</>
 	)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,25 @@ export default function Home() {
 	return (
 		<>
 			<p>
-				Hey gang, I'm Scott and this is my site (obviously). I'm an artist, a programmer, a writer, a designer, a father, a
-				husband, a Jeeper, an overlander, and probably a dozen other things I've forgotten.
+				Hey gang,{' '}
+				<mark className="inline-block rotate-3 bg-red-500 px-1 text-gray-50">
+					<span className="inline-block -rotate-3">I'm Scott</span>
+				</mark>{' '}
+				and this is my site (obviously). I'm an artist, a programmer, a writer, a designer, a father, a husband, a Jeeper, an
+				overlander, and probably a dozen other things I've forgotten.
 			</p>
 			<p>
-				I've got a few drawings, some writing, and other things scattered throughout the site and across various social media
-				platforms. I do hope you find something you enjoy.
+				I've got a few drawings, some writing, and other things{' '}
+				<mark className="inline-block -rotate-1 bg-red-500 px-1 text-gray-50">
+					<span className="inline-block rotate-1">scattered throughout the site</span>
+				</mark>{' '}
+				and across various social media platforms. I do hope you find something you enjoy.
 			</p>
-			<p>Your pal, Scott</p>
+			<p>
+				Your pal,
+				<br />
+				<span className="text-2xl text-gray-900">Scott</span>
+			</p>
 		</>
 	)
 }
