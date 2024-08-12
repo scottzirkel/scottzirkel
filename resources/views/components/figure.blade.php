000c46b17c44d4798e97378dd4a9073a9865a13f
Scott Zirkel <scott@scottzirkel.com>
2024-08-11 21:52:28 -0500

    Fixes broken page

--- resources/views/components/figure.blade.php
+++ resources/views/components/figure.blade.php
@@ -1,7 +1,7 @@
 @inject('storage', 'Illuminate\Support\Facades\Storage')
 <figure class="col-span-1 flex cursor-pointer flex-col items-center justify-between overflow-hidden rounded-lg bg-white shadow hover:shadow-md">
     @isset($image)
-        <img src="{{ $storage::url($image) }}" alt="{{ $title }}" class="aspect-[8/11] object-cover" decoding="async" loading="lazy" width="895" height="250" style="color:transparent;" />
+        <img src="{{ str_starts_with($image, 'http') ? $image : $storage::url($image) }}" alt="{{ $alt ?? $title }}" class="aspect-[8/11] object-cover" decoding="async" loading="lazy" width="895" height="250" style="color:transparent;" />
     @else
         <div class="flex aspect-[8/11] w-full flex-col items-center justify-center bg-gray-600 text-6xl font-bold">?</div>
     @endisset
