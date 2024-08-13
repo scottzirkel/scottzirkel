70ae709bb5b6a74ce5bfd0f810d1a44ac14dd9be
Scott Zirkel <scott@scottzirkel.com>
2024-08-13 15:29:41 -0500

    Missing font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,49 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./resources/views/**/*.blade.php",
-	],
-	theme: {
-		extend: {
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-ellipse': 'radial-gradient(ellipse, var(--tw-gradient-stops))',
-				'gradient-conic': 'conic-gradient(from 54deg, var(--tw-gradient-stops))',
-			},
-			colors: {
-				gray: {
-					100: '#FBFBFA',
-					200: '#F6F5F1',
-					300: '#F0EFE9',
-					400: '#E5E4D9',
-					500: '#DAD8C8',
-					600: '#C4C2B4',
-					700: '#838278',
-					800: '#62615A',
-					900: '#41413C',
-				},
-				red: {
-					100: '#F8ECEC',
-					200: '#EDD0D0',
-					300: '#E2B3B4',
-					400: '#CC7B7B',
-					500: '#B64243',
-					600: '#A43B3C',
-					700: '#6D2828',
-					800: '#521E1E',
-					900: '#371414',
-				},
-			},
-		},
-		fontFamily: {
-			weird: ['"Press Start 2P"', 'display', ...defaultTheme.fontFamily.mono],
-			sans: ['"PT Sans"', ...defaultTheme.fontFamily.sans],
-			title: ['var(--font-free-lunch)', ...defaultTheme.fontFamily.sans],
-			subtitle: ['var(--font-wilco-loft-sans)', ...defaultTheme.fontFamily.sans],
-			small: ['"PT Sans Caption"', ...defaultTheme.fontFamily.sans],
-			mono: ['"PT Mono"', ...defaultTheme.fontFamily.mono],
-		},
-	},
-	plugins: [require('@tailwindcss/typography')],
+    content: [
+        "./resources/views/**/*.blade.php",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-ellipse': 'radial-gradient(ellipse, var(--tw-gradient-stops))',
+                'gradient-conic': 'conic-gradient(from 54deg, var(--tw-gradient-stops))',
+            },
+            colors: {
+                gray: {
+                    100: '#FBFBFA',
+                    200: '#F6F5F1',
+                    300: '#F0EFE9',
+                    400: '#E5E4D9',
+                    500: '#DAD8C8',
+                    600: '#C4C2B4',
+                    700: '#838278',
+                    800: '#62615A',
+                    900: '#41413C',
+                },
+                red: {
+                    100: '#F8ECEC',
+                    200: '#EDD0D0',
+                    300: '#E2B3B4',
+                    400: '#CC7B7B',
+                    500: '#B64243',
+                    600: '#A43B3C',
+                    700: '#6D2828',
+                    800: '#521E1E',
+                    900: '#371414',
+                },
+            },
+        },
+        fontFamily: {
+            weird: ['"Press Start 2P"', ...defaultTheme.fontFamily.mono],
+            sans: ['"PT Sans"', ...defaultTheme.fontFamily.sans],
+            title: ['var(--font-free-lunch)', ...defaultTheme.fontFamily.sans],
+            subtitle: ['var(--font-wilco-loft-sans)', ...defaultTheme.fontFamily.sans],
+            small: ['"PT Sans Caption"', ...defaultTheme.fontFamily.sans],
+            mono: ['"PT Mono"', ...defaultTheme.fontFamily.mono],
+        },
+    },
+    plugins: [require('@tailwindcss/typography')],
 }
