3d7ffee970c8756946e57c039dd48c5c13d77f75
Scott Zirkel <scott@scottzirkel.com>
2024-08-13 16:15:59 -0500

    Self-hosting fonts

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,22 +1,22 @@
 <x-app-layout>
-	<p>
-		Hey gang,
-		<mark class="inline-block rotate-3 bg-red-500 px-1 text-gray-50">
-			<span class="inline-block -rotate-3">I'm Scott</span>
-		</mark>
-		and this is my site (obviously). I'm an artist, a programmer, a writer, a designer, a father of two, a husband of one, a Jeeper, an
-		overlander, and probably a dozen other things I've forgotten.
-	</p>
-	<p>
-		I've got a few drawings, some writing, and other things
-		<mark class="inline-block -rotate-1 bg-red-500 px-1 text-gray-50">
-			<span class="inline-block rotate-1">scattered throughout the site</span>
-		</mark>
-		and across various social media platforms. I do hope you find something you enjoy.
-	</p>
-	<p>
-		Your pal,
-		<br />
-		<strong class="text-2xl">Scott</strong>
-	</p>
+    <p>
+        Hey gang,
+        <mark class="inline-block rotate-3 bg-red-500 px-1 text-gray-50">
+            <span class="inline-block -rotate-3">I'm Scott</span>
+        </mark>
+        and this is my site (obviously). I'm an artist, a programmer, a writer, a designer, a father of two, a husband of one, a Jeeper, an
+        overlander, and probably a dozen other things I've forgotten.
+    </p>
+    <p>
+        I've got a few drawings, some writing, and other things
+        <mark class="inline-block -rotate-1 bg-red-500 px-1 text-gray-50">
+            <span class="inline-block rotate-1">scattered throughout the site</span>
+        </mark>
+        and across various social media platforms. I do hope you find something you enjoy.
+    </p>
+    <p>
+        Your pal,
+        <br />
+        <strong class="text-2xl">Scott</strong>
+    </p>
 </x-app-layout>
