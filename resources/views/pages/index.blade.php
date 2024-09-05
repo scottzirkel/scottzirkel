67657367bbd16d2c8833902becbc32b86aa887e5
Scott Zirkel <scott@scottzirkel.com>
2024-09-05 18:35:00 -0500

    Not much really

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <p>
         Hey gang,
-        <mark class="inline-block rotate-3 bg-red-500 px-1 text-gray-50">
+        <mark class="inline-block px-1 bg-red-500 rotate-3 text-gray-50">
             <span class="inline-block -rotate-3">I'm Scott</span>
         </mark>
         and this is my site (obviously). I'm an artist, a programmer, a writer, a designer, a father of two, a husband of one, a Jeeper, an
@@ -9,7 +9,7 @@
     </p>
     <p>
         I've got a few drawings, some writing, and other things
-        <mark class="inline-block -rotate-1 bg-red-500 px-1 text-gray-50">
+        <mark class="inline-block px-1 bg-red-500 -rotate-1 text-gray-50">
             <span class="inline-block rotate-1">scattered throughout the site</span>
         </mark>
         and across various social media platforms. I do hope you find something you enjoy.
